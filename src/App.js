bffedcdc5a2fb4369d05c8aa5c14b7f97d433813
Tere Diaz <teredr.mx@gmail.com>
2024-11-09 15:52:00 -0600

    Added analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./index.css";
 import AlienIpsumGenerator from "./components/AlienIpsumGenerator";
+// If you don't deploy to vercel remove the line below and the analytics tag too
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="App">
       <AlienIpsumGenerator />
+      <Analytics />
     </div>
   );
 }
